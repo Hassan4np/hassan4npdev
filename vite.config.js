c251fff10b0fbaf961311c8c04737abb9750774c
Hassan Ali <hassan4np@gmail.com>
2025-10-28 15:11:21 +0600

    s

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,6 @@
 import { defineConfig } from "vite";
-import { vitePlugin as remix } from "@remix-run/dev";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    remix(), // 👈 Remix plugin অবশ্যই দরকার
-    tsconfigPaths(),
-  ],
-    css: {
-    devSourcemap: true,
-}
+  plugins: [react()],
 });
-
-
-// import { defineConfig } from 'vite'
-// import { vitePlugin as remix } from "@remix-run/dev";
-
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   css: {
-//     devSourcemap: true,
-// }
-// })
